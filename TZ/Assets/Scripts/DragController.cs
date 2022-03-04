801e63f3dc141ff6e29d2bf43de3a0d61f637205
FatFatFat1 <48385210+FatFatFat1@users.noreply.github.com>
2022-03-04 16:11:47 +0700

    Drag (работает)

---
+++ TZ/Assets/Scripts/DragController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragController : MonoBehaviour , IDragHandler , IPointerClickHandler
+{
+    public GameObject Player;
+    public Canvas canvas;
+    public Camera Cam;
+    private Vector2 _distant;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        _distant = new Vector2(eventData.position.x , 0);
+        _distant = Cam.ScreenToWorldPoint(_distant);
+        Debug.Log(_distant);
+    }
+    void Update()
+    {
+        Player.transform.localPosition = Vector2.Lerp(Player.transform.localPosition, new Vector2(_distant.x , 0), Time.deltaTime*10f);
+
+    }
+
+    //Drag
+
+
+}
--- TZ/Assets/Scripts/PlayerController.cs
+++ TZ/Assets/Scripts/PlayerController.cs
@@ -3,30 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PlayerController : MonoBehaviour, IBeginDragHandler ,IDragHandler
+public class PlayerController : MonoBehaviour
 {
     public GameObject Player;
     void Update()
     {
-        if(Player.transform.position.x > 6)
+        if (Player.transform.position.x > 6)
         {
-            Player.transform.position= new Vector2(6, 0);
+            Player.transform.position = new Vector2(6, 0);
         }
-        if(Player.transform.position.x < -6)
+        if (Player.transform.position.x < -6)
         {
-            Player.transform.position = new Vector2(-6 ,0);
+            Player.transform.position = new Vector2(-6, 0);
         }
     }
-#if UNITY_IOS || UNITY_ANDROID
-    //Swipe
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        Player.transform.position = Vector2.Lerp(Player.transform.position , new Vector2(eventData.delta.x , 0) , Time.deltaTime);
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-
-    }
-#endif
 }
+
